7dfca6078df7e62cd57224bf2fe74b619351a37f
Zoran Cvijic <cvijic.zoran87@gmail.com>
2020-03-19 18:45:11 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const addBox = document.getElementById( 'infinite-box' );
+const classes = ['box', 'box2','box3','box4','box5'];
+
+randomizeClass = Math.floor(Math.random() * classes.length);
+    
 
 let addMore = function() {
-    for ( let i = 0 ; i < 20 ; i++ ){
+    for ( let i = 0 ; i < 5 ; i++ ){
         let box = document.createElement('div');
-        box.classList.add('box');
+        box.classList.add(classes[randomizeClass]);
         addBox.appendChild(box);
     }
 };
 
 addBox.addEventListener('scroll', function(){
-    if( addBox.scrollTop + addBox.clientHeight < addBox.scrollHeight ) {
+    if( addBox.scrollTop + addBox.clientHeight <= addBox.scrollHeight ) {
         addMore();
     }
 });
 
 addMore();
+
